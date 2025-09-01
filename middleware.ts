33ad5d33afb27bbf5cce8a888eb1d73baf63407d
matiastonello92 <tonellomatias@gmail.com>
2025-09-01 05:02:49 +0200

    chore: update supabase server integration (#21)

--- middleware.ts
+++ middleware.ts
@@ -1,12 +1,1 @@
-import type { NextRequest } from "next/server";
-import { updateSession } from "@/utils/supabase/middleware";
-
-export async function middleware(request: NextRequest) {
-  return updateSession(request);
-}
-
-export const config = {
-  matcher: [
-    '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
-  ],
-};
+export { middleware, config } from "@/utils/supabase/middleware";
